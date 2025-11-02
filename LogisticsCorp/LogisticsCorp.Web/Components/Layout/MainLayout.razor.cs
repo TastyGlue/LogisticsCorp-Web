7c1e3a906f56af39e3c77b754810db8a31f0b1d7
Teodor Georgiev <sitikom2007@gmail.com>
2025-11-02 22:22:38 +0200

    Added NavMenu + Modified MainLayout

--- LogisticsCorp/LogisticsCorp.Web/Components/Layout/MainLayout.razor.cs
+++ LogisticsCorp/LogisticsCorp.Web/Components/Layout/MainLayout.razor.cs
@@ -2,6 +2,15 @@
 
 public partial class MainLayout : LayoutComponentBase
 {
-    private bool isDrawerOpen = false;
+    private bool isDrawerOpen = true;
     private bool IsLoading { get; set; } = false;
+
+    // Page title and subtitle properties (can be set from individual pages)
+    protected string PageTitle { get; set; } = "Dashboard";
+    protected string PageSubtitle { get; set; } = "Welcome to LogisticsCorp";
+
+    private void ToggleDrawer()
+    {
+        isDrawerOpen = !isDrawerOpen;
+    }
 }
