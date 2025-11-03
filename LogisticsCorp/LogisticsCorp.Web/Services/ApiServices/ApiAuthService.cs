fb1ef4f501cf8886520a04e133f6bc8de8382564
Teodor Georgiev <sitikom2007@gmail.com>
2025-11-03 23:28:59 +0200

    Added IApiAuthService [NOT FINISHED]

---
+++ LogisticsCorp/LogisticsCorp.Web/Services/ApiServices/ApiAuthService.cs
@@ -0,0 +1,25 @@
+﻿using LogisticsCorp.Web.Services.ApiServices.Interfaces;
+
+namespace LogisticsCorp.Web.Services.ApiServices;
+
+public class ApiAuthService : IApiAuthService
+{
+    private readonly HttpClientService _httpClientService;
+
+    public ApiAuthService(HttpClientService httpClientService)
+    {
+        _httpClientService = httpClientService;
+    }
+
+    public async Task<CustomResult<string>> LoginWithCredentials(LoginCredentials request)
+    {
+        var client = _httpClientService.CreateApiClient();
+
+        string apiEndpoint = "auth/login";
+
+        var response = await client.PostAsJsonAsync(apiEndpoint, request);
+        var content = await response.Content.ReadAsStringAsync();
+
+        return CustomResultUtils.GetApiResponse<string>(response, content);
+    }
+}
