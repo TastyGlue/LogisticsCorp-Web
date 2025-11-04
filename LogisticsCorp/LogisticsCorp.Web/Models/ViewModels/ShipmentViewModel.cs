e7e9e2be19382968cadb50ea6b46821ff8de772c
Ivaylo H <ivcony10@gmail.com>
2025-11-04 19:08:35 +0200

    Shipment

--- LogisticsCorp/LogisticsCorp.Web/Models/ViewModels/ShipmentViewModel.cs
+++ LogisticsCorp/LogisticsCorp.Web/Models/ViewModels/ShipmentViewModel.cs
@@ -1,40 +1,34 @@
-﻿namespace LogisticsCorp.Web.Models.ViewModels
+﻿using LogisticsCorp.Shared.Models.DTOs;
+
+namespace LogisticsCorp.Web.Models.ViewModels
 {
     public class ShipmentViewModel
     {
         public Guid Id { get; set; }
-
         public Guid SenderId { get; set; }
-        public string? SenderName { get; set; }
-
         public Guid RecipientId { get; set; }
-        public string? RecipientName { get; set; }
-
         public Guid RegisteredByEmployeeId { get; set; }
-        public string? RegisteredByEmployeeName { get; set; }
-
         public Guid? CourierId { get; set; }
-        public string? CourierName { get; set; }
-
         public Guid? OriginOfficeId { get; set; }
-        public string? OriginOfficeName { get; set; }
-
+        public DeliveryType DeliveryType { get; set; }
         public Guid? DestinationOfficeId { get; set; }
-        public string? DestinationOfficeName { get; set; }
-
-        public string DeliveryType { get; set; } = string.Empty;
-
         public string? DeliveryAddress { get; set; }
         public string? DeliveryCity { get; set; }
         public string? DeliveryPostalCode { get; set; }
-
         public decimal Weight { get; set; }
-        public string Description { get; set; } = string.Empty;
+        public string Description { get; set; } = default!;
         public decimal Price { get; set; }
-        public string Status { get; set; } = string.Empty;
-
+        public ShipmentStatus Status { get; set; }
         public DateTime RegisteredOn { get; set; }
         public DateTime? PickedUpOn { get; set; }
         public DateTime? DeliveredOn { get; set; }
+
+        public ClientViewModel? Sender { get; set; } = default!;
+        public ClientViewModel? Recipient { get; set; } = default!;
+        public EmployeeViewModel? RegisteredByEmployee { get; set; } = default!;
+        public EmployeeViewModel? Courier { get; set; } = default!;
+        public OfficeViewModel? OriginOffice { get; set; } = default!;
+        public OfficeViewModel? DestinationOffice { get; set; } = default!;
+        //public ICollection<ShipmentHistoryDto>? History { get; set; } = default!;
     }
 }
