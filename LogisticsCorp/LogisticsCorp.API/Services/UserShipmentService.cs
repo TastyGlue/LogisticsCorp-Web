7e14bff171d64aa43b01c9bc1ae6eaf2ac4acb2d
Teodor Georgiev <sitikom2007@gmail.com>
2025-11-04 20:13:59 +0200

    Fixed UserShipments & UserStateContainer

--- LogisticsCorp/LogisticsCorp.API/Services/UserShipmentService.cs
+++ LogisticsCorp/LogisticsCorp.API/Services/UserShipmentService.cs
@@ -10,7 +10,14 @@
         }
         public async Task<CustomResult> GetAll(Guid id)
         {
-            var shipments = await _context.Shipments.Where(x => x.SenderId == id || x.RecipientId == id).ToListAsync();
+            var shipments = await _context.Shipments
+                .Include(s => s.Sender).ThenInclude(x => x.User)
+                .Include(s => s.Recipient).ThenInclude(x => x.User)
+                .Include(s => s.RegisteredByEmployee).ThenInclude(x => x.User)
+                .Include(s => s.Courier).ThenInclude(x => x.User)
+                .Include(s => s.OriginOffice)
+                .Include(s => s.DestinationOffice)
+                .Where(x => x.SenderId == id || x.RecipientId == id).ToListAsync();
 
             return new CustomResult<IEnumerable<Shipment>>(shipments);
         }
---
+++ LogisticsCorp/LogisticsCorp.Web/Components/Pages/UserShipments.razor.cs
@@ -0,0 +1,35 @@
+﻿namespace LogisticsCorp.Web.Components.Pages
+{
+    public partial class UserShipments : ExtendedComponentBase
+    {
+        [Inject] protected IApiUserShipmentService ApiUserShipmentService { get; set; } = default!;
+
+        protected List<ShipmentDto> Shipments { get; set; } = [];
+
+
+        private bool _isLoading = true;
+
+        protected override async Task OnInitializedAsync()
+        {
+            PageStateService.SetPageInfo("Your shipping", "View all things you've sent or received.");
+
+            var accountId = UserStateContainer.AccountId;
+
+            var result = await ApiUserShipmentService.GetAll(accountId);
+
+            if (result.Succeeded)
+            {
+                Shipments = result.Value.ToList();
+            }
+            else
+            {
+                Notify(
+                    result.Error?.Message ?? "An unexpected error occurred during login.",
+                    Severity.Error
+                );
+            }
+
+            _isLoading = false;
+        }
+    }
+}
