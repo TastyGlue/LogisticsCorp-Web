eba009ffaef6acf8a958dec2a2586a94af1fea91
Ivaylo H <ivcony10@gmail.com>
2025-11-04 20:15:28 +0200

    asd

--- LogisticsCorp/LogisticsCorp.Web/Models/ViewModels/ClientViewModel.cs
+++ LogisticsCorp/LogisticsCorp.Web/Models/ViewModels/ClientViewModel.cs
@@ -4,18 +4,11 @@
     {
         public Guid AccountId { get; set; }
         public AccountViewModel? Account { get; set; }
-
-        public string FirstName { get; set; } = default!;
-        public string LastName { get; set; } = default!;
-
-        public string Email { get; set; } = default!;
         public string? PhoneNumber { get; set; }
 
         public string? Address { get; set; }
         public string? City { get; set; }
         public string? PostalCode { get; set; }
-
-        public bool IsActive { get; set; } = true;
 
         public int SentShipmentsCount { get; set; }
         public int ReceivedShipmentsCount { get; set; }
