d3b83554abfc43691c68b180c43f61ffb052109d
Kristian Denkov <cross.2001@outlook.com>
2025-11-04 21:22:14 +0200

    New homepage design

--- LogisticsCorp/LogisticsCorp.Web/Components/Pages/Home.razor.cs
+++ LogisticsCorp/LogisticsCorp.Web/Components/Pages/Home.razor.cs
@@ -4,6 +4,6 @@ public partial class Home : ExtendedComponentBase
 {
     protected override void OnInitialized()
     {
-        PageStateService.SetPageInfo("Dashboard", "Welcome to LogisticsCorp");
+        
     }
 }
