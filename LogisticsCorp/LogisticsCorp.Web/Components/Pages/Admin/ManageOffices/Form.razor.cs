85792cef972b9250cda480fd9129129eb9459d5e
ivcony <137850354+ivcony@users.noreply.github.com>
2025-11-05 10:10:13 +0200

    Office create page (#31)

---
+++ LogisticsCorp/LogisticsCorp.Web/Components/Pages/Admin/ManageOffices/Form.razor.cs
@@ -0,0 +1,20 @@
+﻿using LogisticsCorp.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace LogisticsCorp.Web.Components.Pages.Admin.ManageOffices
+{
+    public partial class Form : ExtendedComponentBase
+    {
+        [Parameter] public bool IsCreate { get; init; } = true;
+        [Parameter] public OfficeViewModel ViewModel { get; set; } = new();
+        [Parameter] public EventCallback OnValidSubmit { get; set; }
+
+        protected async Task ValidSubmitHandler(EditContext context)
+        {
+            await OnValidSubmit.InvokeAsync();
+        }
+
+        protected void CancelHandler()
+            => NavigationManager.NavigateTo("/manage-offices");
+    }
+}
